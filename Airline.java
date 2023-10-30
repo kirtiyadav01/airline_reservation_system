ea5511748ff5a99ea846eed33735443c9bcffa3a
kirtiyadav01 <142882305+kirtiyadav01@users.noreply.github.com>
2023-10-30 19:55:11 +0530

    Add files via upload

---
+++ Airline.java
@@ -0,0 +1,12 @@
+
+
+package javaapplication.airline;
+
+
+public class Airline {
+
+    public static void main(String[] args) {
+        Open launch = new Open();
+        launch.setVisible(true);
+    }
+}
